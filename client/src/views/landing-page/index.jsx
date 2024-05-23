bcb14f59a0543ff8c4b89b6bc56e880924852c10
seif <seifeddine.aaza@gmail.com>
2024-05-23 01:02:27 +0100

    set landing page

---
+++ client/src/views/landing-page/index.jsx
@@ -0,0 +1,15 @@
+import About from "./about";
+import Features from "./features";
+import Header from "./header";
+
+function LandingPage() {
+  return (
+    <>
+      <Header />
+      <About />
+      <Features />
+    </>
+  );
+}
+
+export default LandingPage;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'yellow': '#FFE027',
+        'green': "#42E000",
+      },
+    },
   },
   plugins: [],
 }
