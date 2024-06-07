3eab105996beaebd099775b2422ffe2995bc1028
seif <seifeddine.aaza@gmail.com>
2024-06-07 02:25:54 +0100

    landing page

--- client/src/views/landing-page/Header.jsx
+++ client/src/views/landing-page/Header.jsx
@@ -50,7 +50,7 @@ function Header() {
       <img
         src="https://images.unsplash.com/photo-1607962837359-5e7e89f86776?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt=""
-        className="absolute inset-0 h-full w-full object-cover md:object-center grayscale opacity-60 "
+        className="absolute inset-0 h-full w-full object-cover md:object-center grayscale opacity-75"
       />
       <span
         className="absolute w-full h-full top-0 left-0 "
