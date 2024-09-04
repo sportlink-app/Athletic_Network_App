631888c3b7683a129bf955791b7aa43600ce1ee2
Simo-Devjs <simodevjs@gmail.com>
2024-09-04 12:55:05 +0100

    Delete server/zozo.pydedede

--- server/zozo.py
+++
@@ -1,9 +0,0 @@
-from app import db, app
-
-with app.app_context():
-    try:
-        db.create_all()  # Create all tables
-        print("Database tables created successfully.")
-    except Exception as e:
-        print(f"Error: {e}")
-
