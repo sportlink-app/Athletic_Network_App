d420789f81c47566d61556f858a88a40ded0fab5
simodevjs <simodevjs@gmail.com>
2024-09-04 12:56:21 +0100

    container

--- server/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
