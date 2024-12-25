2994dc05fca0912e101dcc8b03a905d7f901b240
seif <seifeddine.aaza@gmail.com>
2024-12-25 20:26:17 +0100

    updates

--- client/src/views/account/teams/teams/create-team/CreateTeamForm.jsx
+++ client/src/views/account/teams/teams/create-team/CreateTeamForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Alert, AutoComplete, Button, DatePicker, Input, Spin } from "antd";
 import TextArea from "antd/es/input/TextArea";
 import { ArrowRightOutlined } from "@ant-design/icons";
@@ -8,6 +8,7 @@ import PropTypes from "prop-types";
 import dayjs from "dayjs";
 import { useNavigate } from "react-router-dom";
 import userInfoStore from "../../../../../store/user/userInfoStore";
+import L from "leaflet";
 
 function CreateTeamForm() {
   const {
@@ -50,7 +51,7 @@ function CreateTeamForm() {
   };
 
   const nameInput = (
-    <li className="sm:col-span-3 mt-2 flex flex-col gap-1">
+    <li className="sm:col-span-3 mt-2 flex flex-col gap-2">
       <label className="ml-2 font-medium leading-6 text-gray-900">Name</label>
       <Input
         name="name"
@@ -119,7 +120,7 @@ function CreateTeamForm() {
   };
 
   const sportSelect = (
-    <li className="sm:col-span-3 mt-2 flex flex-col gap-1">
+    <li className="sm:col-span-3 mt-2 flex flex-col gap-2">
       <label className="ml-2 font-medium leading-6 text-gray-900">Sport</label>
       <AutoComplete
         options={options}
@@ -143,7 +144,7 @@ function CreateTeamForm() {
   );
 
   const membersCountInput = (
-    <li className="sm:col-span-3 mt-2 flex flex-col gap-1">
+    <li className="sm:col-span-3 mt-2 flex flex-col gap-2">
       <label className="ml-2 font-medium leading-6 text-gray-900">
         Members Count
       </label>
@@ -167,7 +168,7 @@ function CreateTeamForm() {
   );
 
   const descriptionInput = (
-    <li className="sm:col-span-6 mt-2 flex flex-col gap-1">
+    <li className="sm:col-span-6 mt-2 flex flex-col gap-2">
       <label className="ml-2 font-medium leading-6 text-gray-900">
         Description
       </label>
@@ -177,7 +178,7 @@ function CreateTeamForm() {
         onChange={(e) => handleInputChange(e.target.name, e.target.value)}
         placeholder="Enter the description"
         maxLength={350}
-        autoSize={{ minRows: 2 }}
+        autoSize={{ minRows: 4 }}
         style={{ borderRadius: "15px" }}
       />
     </li>
@@ -206,7 +207,7 @@ function CreateTeamForm() {
   };
 
   const dateInput = (
-    <li className="sm:col-span-3 mt-2 flex flex-col gap-1">
+    <li className="sm:col-span-3 mt-2 flex flex-col gap-2">
       <label className="ml-2 font-medium leading-6 text-gray-900">
         Date & Time
       </label>
@@ -228,6 +229,68 @@ function CreateTeamForm() {
     </li>
   );
 
+  const [search, setSearch] = useState(""); // Input value
+  const [suggestions, setSuggestions] = useState([]); // List of location suggestions
+  const [selectedLocation, setSelectedLocation] = useState(null); // Selected location
+
+  const handleSearch = async (e) => {
+    const value = e.target.value;
+    setSearch(value);
+
+    if (value.length > 2) {
+      try {
+        const response = await fetch(
+          `https://nominatim.openstreetmap.org/search?format=json&q=${value}`
+        );
+        const data = await response.json();
+        setSuggestions(data);
+      } catch (error) {
+        console.error("Error fetching location data:", error);
+      }
+    } else {
+      setSuggestions([]);
+    }
+  };
+
+  const handleSelect = (location) => {
+    setSelectedLocation(location);
+    setSearch(location.display_name); // Update input with selected location
+    setSuggestions([]); // Clear suggestions
+  };
+
+  const locationInput = (
+    <li className="sm:col-span-6 mt-2 flex flex-col gap-2">
+      <label className="ml-2 font-medium leading-6 text-gray-900">
+        Location
+      </label>
+      <input
+        type="text"
+        value={search}
+        onChange={handleSearch}
+        placeholder="Type a location (e.g., street, park)"
+        className="form-input mt-2 p-2 border rounded w-full"
+      />
+      {suggestions.length > 0 && (
+        <ul className="suggestions-list border rounded mt-1 max-h-48 overflow-y-auto">
+          {suggestions.map((item) => (
+            <li
+              key={item.place_id}
+              onClick={() => handleSelect(item)}
+              className="suggestion-item cursor-pointer p-2 hover:bg-gray-100"
+            >
+              {item.display_name}
+            </li>
+          ))}
+        </ul>
+      )}
+      {selectedLocation && (
+        <p className="mt-2">
+          <strong>Selected Location:</strong> {selectedLocation.display_name}
+        </p>
+      )}
+    </li>
+  );
+
   return (
     <form
       onSubmit={handleSubmit}
@@ -240,9 +303,10 @@ function CreateTeamForm() {
         />
       )}
       <ul className="grid grid-cols-1 sm:grid-cols-6 gap-2 md:gap-4">
-        {nameInput}
+        {/* {nameInput}
         {sportSelect}
-        {descriptionInput}
+        {descriptionInput} */}
+        {locationInput}
         {membersCountInput}
         {dateInput}
       </ul>
